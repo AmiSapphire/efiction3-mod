0dbd7e55b78f8e9d98a1c612f900738f8a9ac594
Clarissa Walker <amisapphire@zoho.com>
2026-07-29 21:46:06 -0400

    Add options to change toolbar location in tinyMCE 2 and 3 - from tinyMCE 2.0 x to 3.4.x, it was located at the bottom by default; from 3.5.x onwards, the default was changed to be at the top

--- tinymce/3/init.php
+++ tinymce/3/init.php
@@ -24,6 +24,7 @@ echo "
 		theme_advanced_buttons3_add_before: 'tablecontrols,separator',
 		theme_advanced_buttons3_add: 'advhr',
 		theme_advanced_toolbar_align: 'center',
+		theme_advanced_toolbar_location: 'bottom',
 		theme_advanced_statusbar_location: 'bottom',
 		theme_advanced_path: 'false',
 		editor_deselector: 'mceNoEditor',
