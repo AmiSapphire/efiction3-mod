aa53652f7cf8c272c9afe36bd592de7757c28693
Clarissa Walker <amisapphire@zoho.com>
2026-08-22 18:49:10 -0400

    Even more general cleanup - redundancy also removed from one file

--- docs/config.php
+++ docs/config.php
@@ -6,7 +6,4 @@ $dbpass = "dbpass";
 $sitekey = "";
 $settingsprefix = "settings";
 
-include_once("includes/dbfunctions.php");
-if(!empty($sitekey)) $dbconnect = dbconnect($dbhost, $dbuser,$dbpass, $dbname);
-
 ?>
--- includes/dbfunctions.php
+++ includes/dbfunctions.php
@@ -1,12 +1,9 @@
 <?php
 
-
-
 if(function_exists("mysqli_connect")) {
-
 	include_once(_BASEDIR."includes/mysqli_functions.php");
 	$dbconnect = @dbconnect($dbhost, $dbuser, $dbpass, $dbname);
-}else {
+} else {
 	include(_BASEDIR."languages/en.php"); // Because we haven't selected a language setting yet
 	die(_FATALERROR._NODBFUNCTIONALITY);
 }
