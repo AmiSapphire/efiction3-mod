3255944e3929f60a04afbb9955afdea942276b89
Clarissa Walker <amisapphire@zoho.com>
2026-09-24 10:59:56 -0400

    blocks/shoutbox/admin.php: Formatting changes

--- blocks/shoutbox/admin.php
+++ blocks/shoutbox/admin.php
@@ -51,17 +51,17 @@ else include_once("blocks/shoutbox/en.php");
 		save_blocks($blocks);
 	}
 	$defaults = array("m/d/y h:i a", "d/m/y G:i:s", "m-d-y h:i a", "d-m-y G:i:s", "m.d.y h:i a", "d.m.y G:i:s", "M j, Y h:i a", "M j, Y G:i:s", "d M, Y h:i a", "d M, Y G:i:s");
-	$output .= "<form name='shoutopts' method='POST' id='settingsform' action='admin.php?action=blocks&amp;admin=shoutbox'>\n
-<div><label for='shoutdate'>"._SHOUTDATE.":</label><select name='shoutdate' id='shoutdate'><option value=\"\">"._SELECTONE."</option>";
+	$output .= "<form name='shoutopts' method='POST' id='settingsform' style=\"width: 38em;\" action='admin.php?action=blocks&amp;admin=shoutbox'>\n
+<div><label style=\"margin-top: 0.5em;\" for='shoutdate'>"._SHOUTDATE.":</label><select name='shoutdate' id='shoutdate'><option value=\"\">"._SELECTONE."</option>";
 		foreach($defaults as $d) {
 			$output .= "<option value='$d'".($blocks['shoutbox']['shoutdate'] == $d ? " selected" : "").">".date("$d")."</option>";
 		}
 		$output .= "</select> "._OR." <input type='text' name='customshoutdate' class='textbox' value='".(isset($blocks['shoutbox']['shoutdate']) && !in_array($blocks['shoutbox']['shoutdate'], $defaults) ? $blocks['shoutbox']['shoutdate'] : "")."'> <A HREF='#' class='pophelp'>[?]<span>"._HELP_SHOUTDATEFORMAT."</span></A></div>
-<div><label for='shoutlimit'>"._SHOUTLIMIT.":</label><input type='text' name='shoutlimit' size='1' value='".(isset($blocks['shoutbox']['shoutlimit']) ? $blocks['shoutbox']['shoutlimit'] : "")."'></div>
-<div class='fieldset'><span class='label'>"._GUESTSHOUTS.":</span>\n
+<div><label style=\"margin-top: 0.06em;\" for='shoutlimit'>"._SHOUTLIMIT.":</label><input type='text' name='shoutlimit' size='1' value='".(isset($blocks['shoutbox']['shoutlimit']) ? $blocks['shoutbox']['shoutlimit'] : "")."'></div>
+<div class='fieldset'><span style=\"width: 12em; margin-right: -0.8em;\" class='label'><label for='guestshouts'>"._GUESTSHOUTS.":</label></span>\n
 <input type='radio' name='guestshouts' id='guestshouts"._YES."' value='"._YES."'".(!empty($blocks['shoutbox']['guestshouts'])  ? " checked='checked'" : "")."> <label for='guestshouts"._YES."'>"._YES."</label>\n
 <input type='radio' name='guestshouts' id='guestshouts"._YES."' value='"._NO."'".(empty($blocks['shoutbox']['guestshouts'])  ? " checked='checked'" : "")."> <label for='guestshouts"._NO."'>"._NO."</label>\n</div>\n
-<input type='submit' class='button' name='submitopts' id='submit' value='"._SUBMIT."'></form>";
+<input type='submit' style=\"margin-left: 31.8%; margin-top: 0.3em;\"class='button' name='submitopts' id='submit' value='"._SUBMIT."'></form>";
 $shouts = dbquery("SELECT shouts.*, "._PENNAMEFIELD." as penname FROM ".TABLEPREFIX."fanfiction_shoutbox as shouts LEFT JOIN "._AUTHORTABLE." ON "._UIDFIELD." = shouts.shout_name ORDER BY shout_datestamp DESC");
 $totalshouts = dbnumrows($shouts);
 $shouts = dbquery("SELECT shouts.*, "._PENNAMEFIELD." as penname FROM ".TABLEPREFIX."fanfiction_shoutbox as shouts LEFT JOIN "._AUTHORTABLE." ON "._UIDFIELD." = shouts.shout_name ORDER BY shout_datestamp DESC LIMIT $offset, $itemsperpage");
